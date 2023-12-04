1feb9ddc0f5c747b9c2464204be6074a7ac12712
André Martinez <andremartinez92@gmail.com>
2023-12-04 18:34:01 +0000

    Add material UI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    // https://github.com/mui/material-ui/blob/master/examples/material-ui-nextjs-ts/next.config.js
+    modularizeImports: {
+        '@mui/icons-material': {
+            transform: '@mui/icons-material/{{member}}',
+        },
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
