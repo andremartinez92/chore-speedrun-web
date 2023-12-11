17cc45f8b017496fd52bb4efef0d9f852ede44f8
André Martinez <andremartinez92@gmail.com>
2023-12-11 15:03:51 +0000

    Remove stuff before trying supabase

--- src/app/sign-in/index.tsx
+++
@@ -1,5 +0,0 @@
-const SignInScreen = () => {
-  return <div>Hello!</div>;
-};
-
-export default SignInScreen;
--- src/utils/dateFormat.ts
+++
@@ -1,6 +0,0 @@
-import formatRFC3339 from 'date-fns/formatRFC3339';
-
-export const gqlDateFormat = (date: Date) => {
-  // 2018-11-06T00:52:01.520Z
-  return formatRFC3339(date, { fractionDigits: 3 });
-};
