f2a40a0073936846d7bae540f1639b6a95244a8c
André Martinez <andremartinez92@gmail.com>
2023-12-31 12:57:38 +0000

    Redirect when opening website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-'use client';
-
-import { SIGN_IN_ROUTE } from '@/routes';
+import { getSessionToken } from '@/lib/supabase/get-session-token';
+import createSupabaseServerClient from '@/lib/supabase/server';
+import { CHORES_ROUTE, SIGN_IN_ROUTE } from '@/routes';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createSupabaseServerClient();
+  const token = await getSessionToken(supabase);
+
+  if (token) {
+    redirect(CHORES_ROUTE);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Chore Speedrun</h1>
