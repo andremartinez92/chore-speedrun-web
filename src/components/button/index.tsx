bbd193415a2dc6b9e4a1871fbe6736d345a8619b
André Martinez <andremartinez92@gmail.com>
2024-01-17 16:58:00 +0000

    Clean up some components styles

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,7 +4,12 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { ButtonHTMLAttributes, forwardRef } from 'react';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  cn(
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors',
+    'text-sm font-medium',
+    'disabled:pointer-events-none disabled:opacity-50',
+    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2'
+  ),
   {
     variants: {
       variant: {
