72183203298adc91cb315a6e4ddcd89b6f623fd9
André Martinez <andremartinez92@gmail.com>
2024-02-09 23:44:41 +0100

    Center create chore

--- src/app/create-chore/page.tsx
+++ src/app/create-chore/page.tsx
@@ -6,7 +6,9 @@ const Page = () => {
   return (
     <section className="m-20">
       <h1 className="mb-4">Create chore</h1>
-      <CreateChoreForm />
+      <div className="flex items-center justify-center">
+        <CreateChoreForm />
+      </div>
     </section>
   );
 };
