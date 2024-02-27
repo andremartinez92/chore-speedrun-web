b958afc2a8df796501f520044d9b730828423fe5
André Martinez <andremartinez92@gmail.com>
2024-02-27 11:53:57 +0100

    Upgrade dependencies to fix vite vulnerability

--- src/lib/utils/convert-to-gql-date.ts
+++ src/lib/utils/convert-to-gql-date.ts
@@ -1,6 +1,6 @@
 import { formatRFC3339 } from 'date-fns';
 
 export const convertToGqlDate = (date: Date) => {
-  // 2018-11-06T00:52:01.520Zw
+  // 2018-11-06T00:52:01.520Z
   return formatRFC3339(date, { fractionDigits: 3 });
 };
